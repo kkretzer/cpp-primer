fcb38ad595024dad571432ed517b9a08812c9560
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-10-03 17:26:04 -0500

    ex 2.1

---
+++ ch2/ex2.1.predict_unsigned_conversions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    unsigned u = 10, u2 = 42;
+    std::cout << "next line will be 32" << std::endl;
+    std::cout << u2 - u << std::endl;
+    std::cout << "next line will be the maximum integter minus 32" << std::endl;
+    std::cout << u - u2 << std::endl;
+
+    int i = 10, i2 = 42;
+    std::cout << "next line will be 32" << std::endl;
+    std::cout << i2 - i << std::endl;
+    std::cout << "next line will be -32" << std::endl;
+    std::cout << i - i2 << std::endl;
+    std::cout << "next line will be 0" << std::endl;
+    std::cout << i - u << std::endl;
+    std::cout << "next line will be 0" << std::endl;
+    std::cout << u - i << std::endl;
+}
